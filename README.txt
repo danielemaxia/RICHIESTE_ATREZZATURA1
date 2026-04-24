362e21d7fdde7657f1801ee98b033fe11e37c9c5
danielemaxia <danielemaxia@hotmail.it>
2026-04-24 20:31:32 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,6 @@
+VERSIONE V2 - EXPORT INDEX CORRETTO
+
+Correzione:
+- Salva DATI in HTML legge tutte le celle del foglio DATI prima di esportare.
+- Il file scaricato si chiama sempre index.html.
+- Gli articoli vengono incorporati dentro il nuovo index.html.
---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request, { cache: 'no-store' }).catch(() => caches.match(event.request)));
+});
